3b547588e75467644d59f3b7a7fef563070d07f7
Raghav <duaraghav8@protonmail.com>
2025-05-26 22:46:10 +0200

    remove experimental proxy tool

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,13 +1,8 @@
 package api
 
 import (
-	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/mark3labs/mcp-go/client"
-	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
-	"log"
 )
 
 const V0PathPrefix = "/api/v0"
@@ -29,46 +24,6 @@ func NewServer() *gin.Engine {
 		"0.0.1",
 		server.WithToolCapabilities(true),
 	)
-
-	var h = func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		upstreamServer := "http://127.0.0.1:8000/mcp"
-
-		fmt.Println("Received multiply tool call:", request.Method, request.Params)
-		a := request.GetInt("a", 0)
-		b := request.GetInt("b", 0)
-		fmt.Println(request.Params.Name, a, b)
-
-		mcpClient, err := client.NewStreamableHttpClient(upstreamServer)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create MCP client: %w", err)
-		}
-
-		fmt.Println("Initializing client...")
-		initRequest := mcp.InitializeRequest{}
-		initRequest.Params.ProtocolVersion = mcp.LATEST_PROTOCOL_VERSION
-		initRequest.Params.ClientInfo = mcp.Implementation{
-			Name:    "MCP client using streamable http",
-			Version: "0.0.1",
-		}
-		initRequest.Params.Capabilities = mcp.ClientCapabilities{}
-
-		_, err = mcpClient.Initialize(context.Background(), initRequest)
-		if err != nil {
-			log.Fatalf("Failed to initialize client: %v", err)
-		}
-		fmt.Println("Done initializing client")
-
-		return mcpClient.CallTool(ctx, request)
-	}
-
-	t := mcp.NewTool(
-		"multiply",
-		mcp.WithDescription("Multiplies two numbers"),
-		mcp.WithNumber("a", mcp.Description("First number"), mcp.Required()),
-		mcp.WithNumber("b", mcp.Description("Second number"), mcp.Required()),
-	)
-	mcpServer.AddTool(t, h)
-
 	streamableHttpServer := server.NewStreamableHTTPServer(mcpServer)
 	r.Any("/mcp", gin.WrapH(streamableHttpServer))
 
